2b4f8483f72b95b4f582e8409c34e745ab6f19cf
ignatIgnatov <ignatov_rs@abv.bg>
2024-06-14 16:10:54 +0300

    implement ReduxLayout

---
+++ src/components/common-layout/index.js
@@ -0,0 +1,9 @@
+const { default: ReduxProvider } = require("@/provider");
+
+
+
+async function CommonLayout({ children }) {
+    return <ReduxProvider>{children}</ReduxProvider>
+}
+
+export default CommonLayout;
