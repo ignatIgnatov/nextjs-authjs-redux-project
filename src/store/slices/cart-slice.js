ab2203e0625f7eb89b18aba883b9c87063536df5
ignatIgnatov <ignatov_rs@abv.bg>
2024-06-14 16:03:02 +0300

    implement reduser

---
+++ src/store/slices/cart-slice.js
@@ -0,0 +1,22 @@
+const { createSlice } = require("@reduxjs/toolkit")
+
+
+const initialState = {
+    cartItems: []
+}
+
+const cartSlice = createSlice({
+    name: 'cart',
+    initialState,
+    reducers: {
+        addToCart(state, action) {
+
+        },
+        removeFromCart(state, action) {
+
+        }
+    }
+})
+
+export const { addToCart, removeFromCart } = cartSlice.actions;
+export default cartSlice.reducer;
